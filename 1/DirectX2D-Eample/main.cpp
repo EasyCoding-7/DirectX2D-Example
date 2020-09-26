f1c120a735294d5d1f6a7a0ad7851367b9480bff
kimtaehyung <taehyeong77@naver.com>
2020-09-26 22:35:46 +0900

    1

---
+++ 1/DirectX2D-Eample/main.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Device.h"
+
+void InitScene()
+{
+	// 내용 없음
+}
+
+void DestroyScene()
+{
+	// 내용 없음
+}
+
+void Update()
+{
+	// 내용 없음
+}
+
+void Render()
+{
+	// 내용 없음
+}
